74f61d3ba433f621ff0bdf339c126d08185c5885
Sayeduzzaman <sayeduzzamansajid@gmail.com>
2026-04-28 23:06:52 +0600

    can declare winner

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,16 @@ function Squire({ value, onSquireClick }) {
 const Board = () => {
   const [sqr, setSqr] = useState(Array(9).fill(null));
   const [nextX, setNextX] = useState(true)
+  const winner = CalculateWinner(sqr);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (nextX ? "X" : "O");
+  }
 
   const onSquireClick = (i) => {
-    if (sqr[i]) { return }
+    if (sqr[i]||winner) { return }
     const nextSqr = sqr.slice()
     nextX ? nextSqr[i] = "X" : nextSqr[i] = "O";
     setSqr(nextSqr)
@@ -19,6 +26,7 @@ const Board = () => {
   }
   return (
     <>
+    <div>{status}</div>
       <div className='flex'>
         <Squire onSquireClick={() => onSquireClick(0)} value={sqr[0]} />
         <Squire onSquireClick={() => onSquireClick(1)} value={sqr[1]} />
@@ -40,3 +48,23 @@ const Board = () => {
 };
 
 export default Board;
+
+function CalculateWinner(sqr) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (sqr[a] && sqr[a] === sqr[b] && sqr[a] === sqr[c]) {
+      return sqr[a];
+    }
+  }
+  return null;
+}
